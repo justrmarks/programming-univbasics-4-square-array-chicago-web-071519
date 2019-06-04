e472a223e4078f761d98f9a0bb969c2d0ad65c94
Justice Marks <her@shedaddy.com>
2019-06-04 15:16:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0;
   
   while counter < array.length do
-    array[counter] =  array[counter]**2
+    array[counter] =  array[counter]**2;
     counter++
 end 
 
